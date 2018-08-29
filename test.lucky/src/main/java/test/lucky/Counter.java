e4f37b50def8d84332ad1eea618178afa14fb539
Administrator <Administrator@windows10.microdone.cn>
2018-08-29 09:38:22 +0800

    修改

--- test.lucky/src/main/java/test/lucky/Counter.java
+++ test.lucky/src/main/java/test/lucky/Counter.java
@@ -6,7 +6,7 @@ public class Counter {
     public static void inc() {  
         //这里延迟1毫秒，使得结果明显  
         try {  
-            Thread.sleep(1);  
+            Thread.sleep(1);
         } catch (InterruptedException e) {  
         }  
         count++;  
---
+++ test.lucky/src/main/java/test/lucky/ThreadTest.java
@@ -0,0 +1,11 @@
+package test.lucky;
+
+
+public class ThreadTest {
+
+	public static void main( String[] args ) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
